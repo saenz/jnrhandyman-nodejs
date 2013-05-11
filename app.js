70a5142c0ee8c2dfa1e860b8974ea28338e81890
Jeff Saenz <undertheash@gmail.com>
2013-05-11 15:24:23 -0700

    jade version with app.js file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+ // , test = require('./routes/test')
   , http = require('http')
   , path = require('path');
 
@@ -14,7 +15,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -25,18 +26,21 @@ app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+  app.locals.pretty = true;
+}
+
 // jsaenz
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.send(500, 'Something broke!');
 });
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 app.get('/', routes.index);
+//app.get('/test', test.index);
+app.get('/contactus', routes.contactus);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'J & R Handyman' });
 };
+
+exports.contactus = function(req, res) {
+  res.render('contactus', { title: 'J & R Handyman' });
+};
