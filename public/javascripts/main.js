079ebdfbaba3c5ab72daa28fc35f94bffc94cf3c
Jeff Saenz <undertheash@gmail.com>
2013-05-14 21:49:07 -0700

    new header

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 $(function() {
     $('.video').click(function() {
-        $('.video .iframe').append('<iframe width="425" height="230" type="text/html" src="http://www.youtube.com/embed/U6v2ywBsF_Q?fs=1&autoplay=1&loop=1&version=3&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>');
+        $('.video .iframe').append('<iframe width="405" height="230" type="text/html" src="http://www.youtube.com/embed/U6v2ywBsF_Q?fs=1&autoplay=1&loop=1&version=3&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>');
         $('.video .iframe').fadeIn(700);
         $('.video img').fadeOut(700);
     });
